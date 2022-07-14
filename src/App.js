e86a5e66f21c2c243468606b627bdb33dbb61737
CarlosStrider120 <sherifinho.augusto@gmail.com>
2022-07-14 20:15:07 -0300

    Deleting unused files and some changes to app component

--- src/App.js
+++ src/App.js
@@ -8,26 +8,34 @@ class App extends Component {
     constructor() {
         super();
         this.state = {
-            characters: characters,
+            characters: [],
             searchfield: ''
         }
     }
 
+    componentDidMount() {
+        this.setState({ characters: characters });
+    }
+
     onSearchChange = (e) => {
-        this.setState({ searchfield: e.target.value }); 
+        this.setState({ searchfield: e.target.value });
     }
 
     render() {
         const filteredCharacters = this.state.characters.filter(character => {
             return character.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
-        })
-        return (
-            <div className='tc'>
-                <h1 className='f1 dark-red'>D&D Characters</h1>
-                <SearchBox searchChange={this.onSearchChange}/>
-                <CardList characters={filteredCharacters}/>
-            </div>
-        );
+        });
+        if (this.state.characters.length === 0) {
+            return <h1 className='f2 tc'>Loading...</h1>
+        } else {
+            return (
+                <div className='tc'>
+                    <h1 className='f1 dark-red'>D&D Characters</h1>
+                    <SearchBox searchChange={this.onSearchChange} />
+                    <CardList characters={filteredCharacters} />
+                </div>
+            );
+        }
     }
 }
 
