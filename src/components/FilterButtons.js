394ade737db9ae6ddfce145ecebecc2373961aac
CarlosStrider120 <sherifinho.augusto@gmail.com>
2022-07-16 18:44:13 -0300

    Changing event of click in the buttons to onPointerDown

--- src/components/FilterButtons.js
+++ src/components/FilterButtons.js
@@ -3,8 +3,8 @@ import React from 'react';
 const FilterButtons = ({ filterPCs, filterNPCs }) => {
     return (
         <div style={{width: '150px', margin: '0 auto', marginBottom: '2em'}} className='flex flex-row justify-between'>
-            <button style={{border: 'none'}} className='white-90 b br2 f3 bg-red pa2 grow' onClick={filterPCs}>PCs</button>
-            <button style={{border: 'none'}} className='white-90 b br2 f3 bg-blue pa2 grow' onClick={filterNPCs}>NPCs</button>
+            <button style={{border: 'none'}} className='white-90 b br2 f3 bg-red pa2 grow' onPointerDown={filterPCs}>PCs</button>
+            <button style={{border: 'none'}} className='white-90 b br2 f3 bg-blue pa2 grow' onPointerDown={filterNPCs}>NPCs</button>
         </div>
     );
 }
